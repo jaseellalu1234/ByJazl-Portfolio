6f9156babc3c1ffe6e1a58a3325173d7b94a00ea
jaseellalu1234 <jaseel.vpmpd@gmail.com>
2026-03-09 16:08:18 +0530

    Smooth scroll using lenis

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import Works from "./components/Works";
 import Services from "./components/Services";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
+import { ReactLenis } from "lenis/react";
 
 function App() {
   return (
-    <>
+    <ReactLenis root>
       <Hero />
       <Works />
       <Services />
       <Experience />
       <Contact />
-    </>
+    </ReactLenis>
   );
 }
 
